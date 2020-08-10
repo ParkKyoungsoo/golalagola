8d1c6e5c7a3887b7f40cc02f44704e113cff68a9
변찬석 <bcs4125@naver.com>
2020-08-10 12:50:15 +0900

    S03P12B309-45

--- frontend/src/components/WebModal/MultiCarousel.jsx
+++ frontend/src/components/WebModal/MultiCarousel.jsx
@@ -1,57 +1,60 @@
 import React, { useState, useContext } from 'react';
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import { CommonContext } from '../../context/CommonContext';
+import { Grid } from '@material-ui/core';
 
 const MultiCarousel = () => {
+  const { productDatas, setProductDatas } = useContext(CommonContext);
 
-    const { productDatas, setProductDatas } = useContext(CommonContext);
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 8,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 4,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 3,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
 
-    const responsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 6,
-            slidesToSlide: 3 // optional, default to 1.
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 6,
-            slidesToSlide: 6 // optional, default to 1.
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-            slidesToSlide: 1 // optional, default to 1.
-        }
-
-    };
-
-    return (
-        <>
-            <Carousel
-                swipeable={false}
-                draggable={false}
-                showDots={false}
-                responsive={responsive}
-                ssr={true} // means to render carousel on server-side.
-                infinite={true}
-                autoPlaySpeed={1000}
-                keyBoardControl={true}
-                customTransition="all .5"
-                transitionDuration={500}
-                containerClass="carousel-container"
-                removeArrowOnDeviceType={["tablet", "mobile"]}
-                dotListClass="custom-dot-list-style"
-                itemClass="carousel-item-padding-40-px"
-            >
-                {productDatas.map((TmpData, index) => (
-                    <img src={`../../${TmpData.prod_image}`} alt="Prod_image" style={{ width: "150px", height: "150px" }} />
-                ))}
-
-            </Carousel>
-        </>
-    );
-}
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Carousel
+          swipeable={true}
+          draggable={true}
+          showDots={false}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={['tablet', 'mobile']}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
+          {productDatas.map((TmpData, index) => (
+            <img
+              src={`../../${TmpData.prod_image}`}
+              alt="Prod_image"
+              style={{ width: '150px', height: '150px' }}
+            />
+          ))}
+        </Carousel>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default MultiCarousel;
-
