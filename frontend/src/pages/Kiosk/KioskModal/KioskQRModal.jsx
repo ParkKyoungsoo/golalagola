25e8d317422f0166046ab6a1318eb3cd5046e521
jaegyeongkim <tkwk1205@naver.com>
2020-08-17 17:26:12 +0900

    Update Drawer | S03P13B309-70

--- frontend/src/pages/Kiosk/KioskModal/KioskQRModal.jsx
+++ frontend/src/pages/Kiosk/KioskModal/KioskQRModal.jsx
@@ -6,8 +6,15 @@ const CouponsQR = () => {
   return (
     <>
       <img src="images/QR.jpg" alt="test" />
+      <h1>이용방법!</h1>
+      <div>
+        <h2>1. QR코드를 통해 골라라골라 사이트로 접속한다.</h2>
+        <h2>2. 회원가입을 한다!</h2>
+        <h2>3. 원하는 쿠폰을 담는다.</h2>
+        <h2>4. 결제할때 쿠폰함에 있는 QR코드를 보여준다.</h2>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default CouponsQR
+export default CouponsQR;
