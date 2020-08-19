40f117b82219c411bea6f829ee8dc112b039f1cd
jaegyeongkim <tkwk1205@naver.com>
2020-08-19 12:42:48 +0900

    Update payment | S03P13B309-77

--- frontend/src/pages/Payment/index.jsx
+++ frontend/src/pages/Payment/index.jsx
@@ -1,47 +1,58 @@
 import React, { useState } from 'react';
-import { Button, TextField } from '@material-ui/core';
+import { Button, TextField, Grid } from '@material-ui/core';
 import Axios from 'axios';
 
 const Payment = props => {
   const [readCoupon, setReadCoupon] = useState('');
-  console.log('readc', readCoupon);
+  // console.log('readc', readCoupon);
 
   const readCouponChangeHandler = event => {
     setReadCoupon('');
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setReadCoupon(event.target.value);
   };
 
   const onPaymentHandler = async e => {
-    console.log('sss', readCoupon);
+    // console.log('sss', readCoupon);
     setReadCoupon('');
     Axios.put(readCoupon)
       .then(res => {
-        console.log('resresr', res.data);
+        // console.log('resresr', res.data);
         setReadCoupon('');
       })
       .catch(setReadCoupon(''));
   };
   return (
-    <div>
+    <Grid>
       <input
         // style={{ height: '0px', border: '0px' }}
         type="text"
         onChange={readCouponChangeHandler}
         autoFocus
       ></input>
-
-      <Button
-        variant="contained"
-        // disabled={false}
-        fullWidth={true}
-        color="primary"
-        onClick={onPaymentHandler}
-        className="grid-item-button"
+      <Grid
+        container
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignContent: 'center',
+          height: '95vh',
+        }}
       >
-        결제하기
-      </Button>
-    </div>
+        <Grid item xs={11}>
+          <Button
+            variant="contained"
+            // disabled={false}
+            fullWidth={true}
+            color="primary"
+            onClick={onPaymentHandler}
+            style={{ height: '80vh' }}
+          >
+            <h1 style={{ fontSize: '20vw' }}>결제하기</h1>
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 export default Payment;
